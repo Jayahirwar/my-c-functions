255efd94b7a915ee56295bc4820a9d78da75014f
yagyadeepahirwar <73166322+yagyadeepahirwar@users.noreply.github.com>
2022-10-09 13:05:37 +0530

    Update Functions.c

--- Functions.c
+++ Functions.c
@@ -1,9 +1,15 @@
 // learn c programming language
 #include<stdio.h> // this is pre prosessor command it represent by # "std" mean standerd input and output heading file 
-
+int substraction(int,int);
 int maain(){ // this is the main() funtion it is call by opreting system
   // this is the body of the main function all programme write in this body.
   int a=5;  // int is a data type, "a" is a variable name and 5 is assine in a.
   scanf("%d",&a);
   printf("this is your input value= %d",a);
 }
+
+//substraction tow number
+int sub_tow_number(int a,int b){
+  return(a>b?a-b:b-a;);
+}
+  
